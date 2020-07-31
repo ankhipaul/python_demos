c0188aec2ffc1472fcadbe3b375d4d11b183510f
Ankhi Paul <31250559+ankhipaul@users.noreply.github.com>
2020-07-31 17:12:59 -0400

    Hackkerank problem of minimum bias

---
+++ hackerrank_python_challenges/minimumbias.py
@@ -0,0 +1,58 @@
+"""
+Given an array of few people. We need to partner them in such a way so that the difference between them is minimum.
+Then find the sum of those differences.
+
+Function minimumBias has one input parameter-
+arr: an array of integers
+
+Input Format
+The first line contains an integer denoting , the number of people in the array .
+The second line describes as integers representing the person.
+
+Constraint
+n has to be a multiple of 2 in order to create pairs
+
+Output Format
+Integer : Sum of the differences of each group of partners
+
+Sample Input
+n = 6
+arr = [2,5,4,8,6,1]
+
+Sample Output
+4
+
+Explanation
+2 should be paired with 1 as the minimum difference is 1.
+5 should be paired with 4 as the minimum difference is 1.
+Similarly, 8 should be paired with 6.
+
+Sum of the differences = 1+1+2 ,Hence 4
+"""
+import numpy as np
+
+def minimumBias(arr):
+    arr.sort()
+    div = int(len(arr) / 2)
+
+    cnt = 0
+
+    sub_arr = np.array_split(arr,div)
+
+    for i in list(sub_arr):
+        for j,k in enumerate(i[:-1]):
+
+            diff = i[j+1] - k
+            cnt += diff
+    return cnt
+
+if __name__ == '__main__':
+    t = int(input())
+    arr = []
+
+    for t_itr in range(t):
+        n = int(input())
+        arr.append(n)
+
+    bias = minimumBias(arr)
+    print(bias)
